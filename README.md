4a7849742901dfb038926ca41459884f1786faa1
Scott Doxey <neogeek0101@gmail.com>
2024-05-09 16:04:28 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# create-onlybuild-app
+
+> Setup an [onlybuild](https://github.com/neogeek/onlybuild) app with a single command.
+
+[![Publish](https://github.com/neogeek/create-onlybuild-app/actions/workflows/publish.workflow.yml/badge.svg)](https://github.com/neogeek/create-onlybuild-app/actions/workflows/publish.workflow.yml)
+[![NPM version](https://img.shields.io/npm/v/create-onlybuild-app)](https://www.npmjs.org/package/create-onlybuild-app)
+[![Join the chat at https://discord.gg/nNtFsfd](https://img.shields.io/badge/discord-join%20chat-7289DA.svg)](https://discord.gg/nNtFsfd)
+
+## Usage
+
+```bash
+$ npx create-onlybuild-app@latest
+```
---
+++ bin/index.ts
@@ -0,0 +1,264 @@
+#!/usr/bin/env -S node --no-warnings
+
+import { mkdir, readdir, stat, writeFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { cwd, exit } from 'node:process';
+import { promisify } from 'node:util';
+import child_process from 'node:child_process';
+
+const exec = promisify(child_process.exec);
+
+import chalk from 'chalk';
+
+import inquirer from 'inquirer';
+
+import latestVersion from 'latest-version';
+
+import updateNotifier from 'update-notifier';
+
+import pkg from '../package.json' assert { type: 'json' };
+
+updateNotifier({ pkg }).notify();
+
+const dirExists = async (path: string) => {
+  try {
+    await stat(path);
+
+    return true;
+  } catch {}
+
+  return false;
+};
+
+const isDirEmpty = async (path: string) => {
+  try {
+    const files = await readdir(path);
+
+    if (files.length === 0) {
+      return true;
+    }
+  } catch {}
+
+  return false;
+};
+
+let combinedAnswers: {
+  name: string;
+  typescript: boolean;
+  renderer: 'javascript' | 'javascript-html' | 'react';
+  watch: boolean;
+  serve: boolean;
+} = {
+  name: 'my-app',
+  typescript: false,
+  renderer: 'javascript',
+  watch: true,
+  serve: true
+};
+
+await inquirer
+  .prompt([
+    {
+      name: 'name',
+      message: 'What is your project named?',
+      default: combinedAnswers.name
+    },
+    {
+      type: 'confirm',
+      name: 'typescript',
+      message: `Would you like to use ${chalk.blue('TypeScript')}?`,
+      default: combinedAnswers.typescript
+    }
+  ])
+  .then(
+    async answers => (combinedAnswers = { ...combinedAnswers, ...answers })
+  );
+
+await inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'renderer',
+      message: `How would you like to create pages?`,
+      choices: [
+        {
+          name: `${combinedAnswers.typescript ? 'TypeScript' : 'JavaScript'}`,
+          value: 'javascript'
+        },
+        {
+          name: `${
+            combinedAnswers.typescript ? 'TypeScript' : 'JavaScript'
+          } with the \`html\` string template utility`,
+          value: 'javascript-html'
+        },
+        { name: 'React', value: 'react' }
+      ],
+      default: combinedAnswers.renderer
+    }
+  ])
+  .then(
+    async answers => (combinedAnswers = { ...combinedAnswers, ...answers })
+  );
+
+await inquirer
+  .prompt([
+    {
+      type: 'confirm',
+      name: 'watch',
+      message: 'Do you want to watch for changes?',
+      default: combinedAnswers.watch
+    },
+    {
+      type: 'confirm',
+      name: 'serve',
+      message: 'Do you want to serve files locally?',
+      default: combinedAnswers.serve
+    }
+  ])
+  .then(
+    async answers => (combinedAnswers = { ...combinedAnswers, ...answers })
+  );
+
+const directory = join(cwd(), combinedAnswers.name);
+
+if ((await dirExists(directory)) && !(await isDirEmpty(directory))) {
+  console.log(`${directory} is not empty!`);
+
+  exit(1);
+}
+
+const devDependencies: { [key: string]: string } = {
+  onlybuild: await latestVersion('onlybuild')
+};
+
+if (combinedAnswers.renderer === 'react') {
+  devDependencies['react'] = await latestVersion('react');
+  devDependencies['react-dom'] = await latestVersion('react-dom');
+}
+
+console.log(`\nCreating a new onlybuild app in ${chalk.green(directory)}\n`);
+
+await mkdir(directory, { recursive: true });
+
+await mkdir(join(directory, '.vscode/'), { recursive: true });
+
+await writeFile(
+  join(directory, '.vscode/extensions.json'),
+  `${JSON.stringify(
+    {
+      recommendations: [
+        combinedAnswers.renderer === 'javascript-html'
+          ? 'bierner.lit-html'
+          : undefined,
+        'esbenp.prettier-vscode'
+      ].filter(Boolean)
+    },
+    null,
+    2
+  )}\n`
+);
+
+await writeFile(
+  join(directory, '.gitignore'),
+  `${['node_modules/', 'build/'].join('\n')}\n`
+);
+
+await writeFile(join(directory, '.onlyignore'), `${['*.md'].join('\n')}\n`);
+
+const indexContents = (() => {
+  switch (combinedAnswers.renderer) {
+    case 'javascript-html':
+      return `import { html } from 'onlybuild';
+
+export default html\`<h1>Hello, world!</h1>\`;\n`;
+    case 'react':
+      return `import React from 'react';
+import { renderToString } from 'react-dom/server';
+
+function Hello() {
+return <h1>Hello, React!</h1>;
+}
+
+export default renderToString(<Hello />);\n`;
+    default:
+      return `export default '<h1>Hello, world!</h1>';\n`;
+  }
+})();
+
+await writeFile(
+  join(
+    directory,
+    combinedAnswers.typescript
+      ? combinedAnswers.renderer === 'react'
+        ? 'index.tsx'
+        : 'index.ts'
+      : combinedAnswers.renderer === 'react'
+      ? 'index.jsx'
+      : 'index.mjs'
+  ),
+  indexContents
+);
+
+await writeFile(
+  join(directory, 'package.json'),
+  `${JSON.stringify(
+    {
+      name: combinedAnswers.name,
+      type:
+        combinedAnswers.typescript || combinedAnswers.renderer === 'react'
+          ? 'module'
+          : undefined,
+      devDependencies,
+      scripts: {
+        build: 'onlybuild',
+        watch: combinedAnswers.watch
+          ? 'npx nodemon --ignore ./build -x "npm run build"'
+          : undefined,
+        serve: combinedAnswers.serve ? 'npx http-server build' : undefined
+      },
+      private: true
+    },
+    null,
+    2
+  )}\n`
+);
+
+await writeFile(join(directory, 'README.md'), `# ${combinedAnswers.name}\n`);
+
+console.log(
+  `Installing devDependencies:\n${Object.keys(devDependencies)
+    .map(dependency => `- ${chalk.blue(dependency)}`)
+    .join('\n')}\n`
+);
+
+await exec(`cd ${directory} && npm install`);
+
+console.log('Initializing a git repository.\n');
+
+await exec(
+  `cd ${directory} && git init && git add . && git commit -m "Initial commit."`
+);
+
+console.log(
+  `${chalk.green('Success!')} Created ${combinedAnswers.name} in ${directory}\n`
+);
+
+console.log('Next steps:\n');
+console.log(`cd ${combinedAnswers.name}/\n`);
+console.log(
+  `npm run build ${chalk.gray('# Build project to ./build directory.')}`
+);
+
+if (combinedAnswers.watch) {
+  console.log(
+    `npm run watch ${chalk.gray(
+      '# Watch for changes and rebuild when detected.'
+    )}`
+  );
+}
+
+if (combinedAnswers.serve) {
+  console.log(
+    `npm run serve ${chalk.gray('# Serve ./build directory locally.')}`
+  );
+}
